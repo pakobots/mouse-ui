6fe436bc9333bee5cd5e9246a04a06be413a7fc9
Michael Aumock <maumock@origami3.com>
2017-08-19 04:58:21 -0600

    package.json updated with targets for building. Allows others without nps installed to build.

--- phone-prod.sh
+++ phone-prod.sh
@@ -2,7 +2,7 @@
 cordova clean
 cordova-hcp build
 cordova build android --release
-find ./platforms/android/build/outputs/apk/*release* -exec jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../security/android.keystore {} apps \;
+find ./platforms/android/build/outputs/apk/*release* -exec jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../phone/security/android.keystore {} apps \;
 mkdir -p release
 rm release/*.apk
 find ./platforms/android/build/outputs/apk/*release* -exec cp {} ./release \;
