37f7895c248af31f908ab887f71d26988cff106e
Michael Aumock <maumock@origami3.com>
2018-02-06 05:12:59 -0700

    Adding lame apple logo 1024x1024. Unable to upload app until this is added. What nonsense ;)

--- phone-prod.sh
+++ phone-prod.sh
@@ -2,10 +2,10 @@
 cordova clean
 cordova-hcp build
 cordova build android --release
-find ./platforms/android/build/outputs/apk/*release* -exec jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../phone/security/android.keystore {} pakobots-key \;
-mkdir -p release
+sleep 1
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../phone/security/android.keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk pakobots-key
 rm ./release/*.apk
-find ./platforms/android/build/outputs/apk/*release* -exec cp {} ./release \;
+cp ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./release
 rename s/-unsigned// ./release/*.apk
 zipalign -fv 4 ./release/*.apk ./release/final.apk
 zipalign -cv 4 ./release/final.apk
